8ec7bb6b8678bc9a01a1b1ac599fd123db163b1b
Andrea Landi <andrea.landi@ark-energy.eu>
2023-12-29 17:08:30 +0100

    Add setupListeners to configureStore

--- src/app/configureStore.ts
+++ src/app/configureStore.ts
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import notificationsReducer from "../features/notifications/notification";
 import errorReducer from "../features/errorHandler/errorHandler";
 import { jsonPlaceholderSlice } from "../features/jsonPlaceholderAPI/jsonPlaceholderSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
   reducer: {
@@ -18,3 +19,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+setupListeners(store.dispatch);
