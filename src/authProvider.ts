721534dde6cf5e9149d1418461826aa7921e22cf
Andrea Cuneo <andrea.cuneo@ark-energy.eu>
2024-06-13 18:12:31 +0200

    chore: remove unused file

--- src/authProvider.ts
+++
@@ -1,5 +0,0 @@
-import { env } from "./config/env";
-import { Auth0AuthProvider } from "./lib/authentication/providers/auth0AuthProvider";
-import type { AuthProvider } from "./lib/authentication/providers/authProviderInterface";
-
-export const authProvider: AuthProvider = new Auth0AuthProvider(env);
