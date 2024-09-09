b4031eac61dab5ececa7e3c0922137dbf13e7803
AndreaLandiArk <andrea.landi@ark-energy.eu>
2024-09-09 17:36:19 +0200

    Added Zod schema for header building in tables. Added better error handling for tables

--- src/components/tables/plainTable/chackraPlainTable.tsx
+++ src/components/tables/plainTable/chackraPlainTable.tsx
@@ -1,73 +1,85 @@
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  TableContainer,
-  Center,
-  Spinner,
-} from '@chakra-ui/react'
+import { Center, Spinner, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import type { SerializedError } from "@reduxjs/toolkit";
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import type { z, ZodObject, ZodRawShape } from "zod";
 
-export type PlainTablePropsType<T> = {
-  data: T[]
-  colorscheme?: string
-  variant?: string
-  isLoading: boolean
-  isError: boolean
-}
+import type { DetailsType } from "../../../lib/errorHandler/errorHandler";
+import { setError } from "../../../lib/errorHandler/errorHandler";
+import { isErrorWithMessage, isFetchBaseQueryError } from "../../../lib/errorHandler/errorHandlingLib";
 
-export const ChackraPlainTable = <T extends object,>({
+export type PlainTablePropsType<T extends ZodRawShape> = {
+  data: z.infer<ZodObject<T>>[];
+  colorscheme?: string;
+  variant?: string;
+  isLoading: boolean;
+  isError: boolean;
+  schema: ZodObject<T>;
+  error: FetchBaseQueryError | SerializedError | undefined;
+};
+
+export const ChackraPlainTable = <T extends ZodRawShape>({
   data,
-  colorscheme = 'grey',
-  variant = 'striped',
+  colorscheme = "grey",
+  variant = "striped",
   isLoading,
   isError,
+  error,
+  schema,
 }: PlainTablePropsType<T>) => {
-  type TK = keyof T
-  const first = data.length < 1 ? {} : data[0];
-
-  /* TODO: this doesn't work well as
-   * Typescript doesn't support compile-time to derive the properties of T
-   * At runtime, the Array may be empty and thus cannot be derived either
-   * Solutions:
-   *   1. move responsability to developer
-   *   2. use Zod schema, pairing T with Schema: z.object({...}).shape contains the list of keys
-   *      requires time to understand the right Generics to use 
-   */
-  const headers = Object.keys(first) as TK[]
-
-  return (
-    <TableContainer my="30px">
-      <Table variant={variant} colorScheme={colorscheme}>
-        <Thead>
-          <Tr>
-            {headers.map((header) => (
-              <Th key={String(header)}>{String(header)}</Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody>
-          {isLoading || isError ? (
+  if (isError && error) {
+    let details = {} as DetailsType;
+    if (isFetchBaseQueryError(error)) {
+      details = {
+        title: "Fetching Error",
+        message: "error" in error ? error.error : JSON.stringify(error.data),
+        status: error.status,
+        isValidationError: false,
+        exceptionDetails: null,
+      } as DetailsType;
+    } else if (isErrorWithMessage(error)) {
+      // you can access a string 'message' property here
+      details = {
+        title: "Fetching Error",
+        message: error.message,
+        isValidationError: false,
+        exceptionDetails: null,
+      } as DetailsType;
+    }
+    setError({ error: true, details });
+    return <></>;
+  } else {
+    const headers = Object.keys(schema.shape) as (keyof T)[];
+    return (
+      <TableContainer my="30px">
+        <Table variant={variant} colorScheme={colorscheme}>
+          <Thead>
             <Tr>
-              <Td colSpan={headers.length}>
-                <Center>
-                  <Spinner />
-                </Center>
-              </Td>
+              {headers.map(header => (
+                <Th key={String(header)}>{String(header)}</Th>
+              ))}
             </Tr>
-          ) : (
-            data.map((row, rowIndex) => (
-              <Tr key={rowIndex}>
-                {headers.map((header) => (
-                  <Td key={String(header)}>{row[header] as any}</Td> // eslint-disable-line @typescript-eslint/no-explicit-any
-                ))}
+          </Thead>
+          <Tbody>
+            {isLoading ? (
+              <Tr>
+                <Td colSpan={headers.length}>
+                  <Center>
+                    <Spinner />
+                  </Center>
+                </Td>
               </Tr>
-            ))
-          )}
-        </Tbody>
-      </Table>
-    </TableContainer>
-  )
-}
+            ) : (
+              data.map((row, rowIndex) => (
+                <Tr key={rowIndex}>
+                  {headers.map(header => (
+                    <Td key={String(header)}>{row[header] as any}</Td> // eslint-disable-line @typescript-eslint/no-explicit-any
+                  ))}
+                </Tr>
+              ))
+            )}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    );
+  }
+};
--- src/features/fetchApiExample/JsonPlaceholderPostsTable.tsx
+++ src/features/fetchApiExample/JsonPlaceholderPostsTable.tsx
@@ -1,15 +1,21 @@
 import { Heading } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import { z } from "zod";
 
 import { ChackraPlainTable } from "../../components/tables/plainTable/chackraPlainTable";
 
 import { useFetchPostsQuery } from "./jsonPlaceholderSlice";
+
+export const PostDataSchema = z.object({
+  id: z.number().nullable(),
+  userId: z.number().nullable(),
+  title: z.string().nullable(),
+  body: z.string().nullable(),
+});
+
+export type PostDataType = z.infer<typeof PostDataSchema>;
 const JsonPlaceholderPostsTable = () => {
-  const {
-    data,
-    isLoading,
-    isError,
-  } /*This also contains error and isSuccess*/ = useFetchPostsQuery(null, {
+  const { data, isLoading, isError, error } /*This also contains error and isSuccess*/ = useFetchPostsQuery(null, {
     pollingInterval: 30000,
     refetchOnReconnect: true,
     refetchOnMountOrArgChange: true,
@@ -25,9 +31,11 @@ const JsonPlaceholderPostsTable = () => {
         data={data || []}
         isLoading={isLoading}
         isError={isError}
+        error={error}
+        schema={PostDataSchema}
       />
     </>
-  )
-}
+  );
+};
 
-export default JsonPlaceholderPostsTable
+export default JsonPlaceholderPostsTable;
