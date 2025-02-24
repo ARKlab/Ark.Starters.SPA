19766ed09a6d4439ec4cef87ae29fed1a49d0228
Mirko <mirko.petrelli@ark-energy.eu>
2025-02-24 12:37:02 +0100

    Fix debounced state
    (cherry picked from commit d8336662e68927ae5dfafb79ecdd7758670b3d80)

--- src/components/debouncedInputColumnHeader.tsx
+++ src/components/debouncedInputColumnHeader.tsx
@@ -1,35 +1,37 @@
 
-import { useEffect, useState } from "react";
 
-import useDebounce from "../lib/useDebounce";
+import { useEffect } from "react";
+
+import { useDebouncedState } from "../lib/useDebounce";
 
 import { ChackraInputHeaderFilterWithClear } from "./chackraInputFilterWithClear/chackraInputHeaderFilterWithClear";
+
+type DebouncedInputColumnHeaderProps = {
+  value: string | number;
+  onChange: (value: string | number) => void;
+  debounce?: number;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange" | "size">;
 
 export function DebouncedInputColumnHeader({
   value: initialValue,
   onChange,
   debounce = 500,
   ...props
-}: {
-  value: string | number;
-  onChange: (value: string | number) => void;
-  debounce?: number;
-} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange" | "size">) {
-  const [value, setValue] = useState(initialValue);
-  const debounceValue = useDebounce(value, debounce);
+}: DebouncedInputColumnHeaderProps) {
+
+  const [value, setValue] = useDebouncedState(initialValue, debounce);
 
   useEffect(() => {
-    setValue(initialValue)
-  }, [initialValue])
-
-  useEffect(() => {
-    onChange(debounceValue);
-  }, [debounceValue, onChange]);
+    onChange(value);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value]);
 
   return (
     <ChackraInputHeaderFilterWithClear
       value={value}
-      onChange={(e) => { setValue(e.target.value); }}
+      onChange={(e) => {
+        setValue(e.target.value);
+      }}
       {...props}
     />
   )
